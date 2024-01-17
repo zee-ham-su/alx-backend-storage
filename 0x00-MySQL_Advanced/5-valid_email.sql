be5841db029a68840e55861a780aa18541ff98d5
zee-ham-su <hsufiian@yahoo.com>
2024-01-17 19:10:04 +0000

    validate

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,11 +4,11 @@
 
 DELIMITER //
 
-CREATE TRIGGER before_update_email
+CREATE TRIGGER validate
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
+    IF NEW.email != OLD.email THEN
         SET NEW.valid_email = NULL;
     END IF;
 END;
