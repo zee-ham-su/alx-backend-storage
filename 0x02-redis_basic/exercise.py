72a01585029d799cadcbc5e3701a39e25044e4b7
zee-ham-su <hsufiian@yahoo.com>
2024-01-24 13:10:59 +0000

    Implement Redis data retrieval with type recovery

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 """
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -23,3 +23,23 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return (key)
+
+    def get(self, key: str, fn: Callable = None) -> Union[str,
+                                                          bytes, int, None]:
+        """ Fetches information from Redis using the specified
+        key and optionally employs a conversion function.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> Union[str, bytes, None]:
+        """ Fetches a string data from Redis using the given key
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """ Fetches integer data from Redis using the given key.
+        """
+        return self.get(key, fn=int)
