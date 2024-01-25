fd5c80df1af860a61ee3d9b973f463fb7546f815
zee-ham-su <hsufiian@yahoo.com>
2024-01-25 11:33:59 +0000

    cache

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -15,16 +15,19 @@ def count_url_access(method):
     def wrapper(url):
         cache_key = "cache:" + url
         cached_content = store.get(cache_key)
+
         if cached_content:
             return cached_content.decode("utf-8")
 
         access_count_key = "access_count:" + url
+        store.incr(access_count_key)  # Increment access count
+
         html_content = method(url)
 
-        store.incr(access_count_key)
         store.set(cache_key, html_content)
-        store.expire(cache_key, 10)
+        store.expire(cache_key, 10)  # Set cache expiration time
         return html_content
+
     return wrapper
 
 
